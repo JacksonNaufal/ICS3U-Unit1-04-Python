a1115b86928f0bc4ebc210fb6d0b6a6b37fe7e5a
Jackson Naufal <Jackson.naufal@mths.ca>
2022-02-24 19:35:30 +0000

    AreaPerimeterCalculator

---
+++ area_perimeter_calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Jackson Naufal
+# Created on: Feb 2022
+# This is a Area and perimeter calculator with the dimensions of 5cm x 3cm
+
+
+def main():
+    print("If your rectangle has the dimensions")
+    print("5cm x 3cm")
+    print("")
+    print("The area of your rectangle is {}cm.".format(3 * 5))
+    print("The perimeter of your rectangle is {}cm".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
